5c5270fd73d0b8f5cc4ee7af01477dd1c95bcbd9
Arthur Silva <75430972+ArthurSenju@users.noreply.github.com>
2024-02-20 11:42:22 -0300

    finalizacao do responsivo

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ carouselItems.style.width = `${totalItems * itemWidth}px`;
 function nextSlide() {
   currentIndex = (currentIndex + 1) % totalItems;
   updateCarousel();
+  console.log(currentIndex)
 }
 
 function prevSlide() {
@@ -34,3 +35,42 @@ document.querySelector('.prev').addEventListener('click', prevSlide);
 
 // Mostrar o primeiro item inicialmente
 updateCarousel();
+
+const carousel2 = document.querySelector('.carousel2');
+const carouselItems2 = document.querySelector('.carousel-items2');
+const items2 = document.querySelectorAll('.item2');
+
+let currentIndex2 = 0;
+const totalItems2 = items2.length;
+const itemWidth2 = items2[0].offsetWidth;
+
+carouselItems2.style.width = `${totalItems2 * itemWidth2}px`;
+
+function proximaSlide() {
+  currentIndex2 = (currentIndex2 + 1) % totalItems2;
+  updateCarousel2();
+  console.log(currentIndex2)
+}
+
+function antesSlide() {
+  currentIndex2 = (currentIndex2 - 1 + totalItems2) % totalItems2;
+  updateCarousel2();
+}
+
+function updateCarousel2() {
+  // Remove todos os itens do carrossel
+  while (carouselItems2.firstChild) {
+    carouselItems2.removeChild(carouselItems2.firstChild);
+  }
+
+  // Adiciona o item atual ao carrossel
+  const currentItem2 = items2[currentIndex2].cloneNode(true);
+  carouselItems2.appendChild(currentItem2);
+}
+
+document.querySelector('.anterior').addEventListener('click', proximaSlide);
+document.querySelector('.posterior').addEventListener('click', antesSlide);
+
+// Mostrar o primeiro item inicialmente
+updateCarousel2();
+
